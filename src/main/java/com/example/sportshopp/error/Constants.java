5fdffeca1602e07a1ba2dcc7515bd55389ac1d5e
mvw21 <53233579+mvw21@users.noreply.github.com>
2020-08-07 17:47:36 +0300

    Update Constants.java

--- src/main/java/com/example/sportshopp/error/Constants.java
+++ src/main/java/com/example/sportshopp/error/Constants.java
@@ -7,12 +7,8 @@ public class Constants {
 
     public final static String PASSWORD_IS_INCORRECT = "Incorrect password";
 
-    public final static String VIDEO_NOT_FOUND = "Video not found";
+    public final static String PRODUCT_NOT_FOUND = "Product not found";
 
-    public final static String VIDEO_ID_NOT_FOUND = "Video with this id does not exists";
-
-    public final static String QUOTE_NOT_FOUND = "Quote not found";
-
-    public final static String QUOTE_ID_NOT_FOUND = "Quote with this id does not exists";
+    public final static String PRODUCT_ID_NOT_FOUND = "Product with this id does not exists";
 
 }
